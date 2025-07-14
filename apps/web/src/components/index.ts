618d3ec63c47b179495283153a7ac5a1b64433aa
Yago F. <yagoalmeida@gmail.com>
2025-07-14 19:43:42 -0300

    Adds Shadcn

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,9 @@
+// UI Components
+export * from "./ui/button";
+export * from "./ui/card";
+export * from "./ui/input";
+export * from "./ui/label";
+export * from "./ui/form";
+export * from "./ui/table";
+export * from "./ui/dialog";
+export * from "./ui/dropdown-menu";
---
+++ apps/web/src/hooks/index.ts
@@ -0,0 +1,1 @@
+// Custom hooks will be exported here
