6f4b4a4c1b774c32df97ce711beb876abd52c256
Yago F. <yagoalmeida@gmail.com>
2025-07-16 19:55:05 -0300

    Format .

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,7 @@
 // FIXME: THIS FILE COMES FROM THE SHADCN REPO IT MUST COMPLY TO PREVIOUS STANDARDS
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@workspace/ui"],
 };
 
 export default nextConfig;
